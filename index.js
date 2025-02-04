6958b8ff20333066a3662bc582fed923e243c2d0
patrickw24 <patrick.warner24@gmail.com>
2025-02-03 19:04:04 -0600

    updated values

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/users/:id', async function (req, res){
 app.post('/users', async function (req, res){
 
     const tmp= req.body
-    const str= 'insert into users (name, email) values (Patrick, patrick@codexacademy.com)'
+    const str= 'insert into users (name, email) values ($1, $2)'
     const arr = [tmp.name, tmp.email]
     const result = await db.default.query(str, arr)
     res.status(200).json({message:"User Added"})
@@ -36,7 +36,7 @@ app.put('/users/:id', async (req, res) =>{
     const tmp= req.body
     
     const arr= [tmp.name, tmp.email, id_users]
-    const sql= 'UPDATE users set name = Sam, email= sam@codexacademy.com where id_users= 1'
+    const sql= 'UPDATE users set name = $1, email= $2 where id_users= $3'
 
     const result= db.default.query(sql, arr)
 
@@ -47,7 +47,7 @@ app.put('/users/:id', async (req, res) =>{
 app.delete('/users/:id', async(req)=>{
 
     const id_users= req.params.id
-    const sql= 'DELETE from users where id_users= 1'
+    const sql= 'DELETE from users where id_users= $1'
     const arr= [id_users]
 
     const result = await db.default.query(sql,arr)
@@ -75,7 +75,7 @@ app.get('/products/:id', async(req, res)=>{
 
 app.post('/products', async(req, res)=>{
 
-    const sql= 'insert into products (name, description, price, stock) values (iphone, cellphone, 20, 23)'
+    const sql= 'insert into products (name, description, price, stock) values ($1, $2, $3, $4)'
     const tmp= req.body
     const arr= [tmp.name, tmp.description, tmp.price, tmp.stock]
 
@@ -91,11 +91,11 @@ app.put('/products/:id', async(req, res)=>{
     const arr = [tmp.name, tmp.description, tmp.price, tmp.stock, id_products]
 
     const sql = `update products 
-    set name= samsungtv,
-        description= tv,
-        price = $5000,
-        stock = 10
-        where id_products= 1
+    set name= $1,
+        description= $2,
+        price = $3,
+        stock = $4
+        where id_products= $5
      `
 
      const result = db.default.query(sql, arr)
@@ -107,7 +107,7 @@ app.put('/products/:id', async(req, res)=>{
 app.delete('/products/:id', async (req,res)=>{
 
         const id_products= req.params.id
-        const sql = 'delete from products where id_products= 1'
+        const sql = 'delete from products where id_products= $1'
         const arr= [id_products]
 
         const result = await db.default.query(sql,arr)
